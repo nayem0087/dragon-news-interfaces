7e3cbfafbf1823746235da99abe52a5b9728579f
Nayem Ahmed <nayemk0087@gmail.com>
2026-04-27 18:07:22 +0600

    added my dynamic google font $$ dynamic date in my interface

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,10 +1,11 @@
 import Header from '@/components/shared/Header';
 import Navbar from '@/components/shared/Navbar';
 import React from 'react';
+import { montserrat } from '../layout';
 
 const MainLayout = ({children}) => {
     return (
-        <div>
+        <div className={`${montserrat.className}`}>
             <Header />
             <Navbar />
             {children}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700']
+})
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
   weight: ['400', '500', '600', '700']
 })
@@ -27,7 +23,7 @@ export default function RootLayout({ children }) {
     <html
       data-theme="light"
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`h-full antialiased`}
     >
       <body className={`${poppins.className} min-h-full flex flex-col`} suppressHydrationWarning>
           {children}
