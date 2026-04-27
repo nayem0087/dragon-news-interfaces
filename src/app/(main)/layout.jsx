a6b1084cf7aa5e2b6a0db6a4a8f8df91554e39fd
Nayem Ahmed <nayemk0087@gmail.com>
2026-04-27 18:27:12 +0600

    added my dynamic news deedline in my header

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -2,11 +2,13 @@ import Header from '@/components/shared/Header';
 import Navbar from '@/components/shared/Navbar';
 import React from 'react';
 import { montserrat } from '../layout';
+import BreakingNews from '@/components/shared/BreakingNews';
 
 const MainLayout = ({children}) => {
     return (
         <div className={`${montserrat.className}`}>
             <Header />
+            <BreakingNews/>
             <Navbar />
             {children}
         </div>
---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const news = [
+    {
+        _id: "1",
+        title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+        _id: "2",
+        title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+        _id: "3",
+        title: "Breaking News: Major Event Unfolds in the City",
+    },
+]
+
+const BreakingNews = () => {
+    return (
+        <div className='max-w-[80%] mx-auto flex justify-between gap-4 items-center bg-gray-200 p-4 rounded-[5px]'>
+            <button className='btn bg-red-500 text-white'>Latest News</button>
+            <Marquee>
+               {news.map((n) => ( 
+                 <span key={n._id}>{n.title}</span>
+               ))}
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
